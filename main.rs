9d9ace74e0ca54b1b5deaa566ec9fab4fb02ee41
Sagi Kanfi <sagikan5@gmail.com>
2025-08-18 13:34:56 +0300

    Handle concurrent clients

--- main.rs
+++ main.rs
@@ -1,27 +1,29 @@
 #![allow(unused_imports)]
-use std::io::{Read, Write};
-use std::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() {
+    // Set listener to port 6379
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut buf = vec![0; 1024];
-                loop {
-                    let read_count = stream.read(&mut buf).unwrap();
-                    if read_count == 0 {
-                        break;
+    loop {
+        // Accept client (blocking)
+        let (mut socket, _) = listener.accept().await.unwrap();
+
+        // Tokio runtime read-write function
+        tokio::spawn(async move {
+            let mut buf = vec![0; 1024];
+            loop {
+                match socket.read(&mut buf).await {
+                    Ok(0) => return, // Connection closed
+                    Ok(_n) => socket.write_all(b"+PONG\r\n").await.unwrap(),
+                    Err(e) => {
+                        eprintln!("Error: {}", e);
+                        return;
                     }
-                    
-                    stream.write_all(b"+PONG\r\n").unwrap();
                 }
-                
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
 }
